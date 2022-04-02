fff0844a94f8fca00025f80468a5f1172175dd82
aohara-cc <aohara@camcloud.com>
2022-04-02 00:19:14 -0400

    slight modernization; upgrade httpclient

---
+++ src/test/java/org/openid4java/FootTest.java
@@ -0,0 +1,23 @@
+package org.openid4java;
+
+import org.openid4java.consumer.ConsumerManager;
+
+public class FootTest {
+
+    public static void main(String[] args) {
+        try {
+            final var consumer = new ConsumerManager();
+            consumer.setMaxAssocAttempts(0);
+
+            final var discoveries = consumer.discover("http://steamcommunity.com/openid");
+            final var info = consumer.associate(discoveries);
+            System.out.println(info);
+
+            final var authUri = consumer.authenticate(info, "http://localhost:3000/callback");
+            System.out.println(authUri.getDestinationUrl(true));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
+    }
+}
